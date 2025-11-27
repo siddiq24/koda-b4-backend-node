4a705e15b6cae328b02aaa980d398d347cfec4bd
siddiq24 <siddiqrm24@gmail.com>
2025-11-27 17:58:54 +0700

    fix: fixing calling router name

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const authRouter = require('./auth.router');
-const productRouter = require('./product.router');
 const adminRouter = require('./admin.router');
 
 router.use('/admin', adminRouter);
