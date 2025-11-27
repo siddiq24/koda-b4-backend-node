edf3f62e461eaa340c794b1bf9da11c1ef8ffc15
siddiq24 <siddiqrm24@gmail.com>
2025-11-27 15:27:51 +0700

    feat: add manajement database admin products

---
+++ src/models/admin.model.js
@@ -0,0 +1,52 @@
+const prisma = require("../libs/prisma");
+
+module.exports = {
+    // ============================
+    createProduct: async ({ title, description, basePrice, stock, categoryId }) => {
+        return await prisma.products.create({
+            data: {
+                title,
+                description,
+                basePrice,
+                stock,
+                categoryId,
+            },
+        });
+    },
+
+    // ============================
+    getAllProducts: async () => {
+        return await prisma.products.findMany();
+    },
+
+    // ============================
+    getProductById: async (id) => {
+        return await prisma.products.findUnique({
+            where: { id },
+            include: {
+                category: true,
+                sizes: {
+                    include: { size: true }
+                },
+                images: true,
+            },
+        });
+    },
+
+    // ============================
+    productIsExist: async (title) => {
+        const product = await prisma.products.findUnique({
+            where: { title },
+            select: { id: true },
+        });
+        return !!product;
+    },
+
+    // ============================
+    updateProduct: async (id, data) => {
+        return await prisma.products.update({
+            where: { id },
+            data: data,
+        });
+    },
+};
