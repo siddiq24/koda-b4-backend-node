30e4d1d8e21e8b8e55f1c706656947cabee63a9b
siddiq24 <siddiqrm24@gmail.com>
2025-11-30 07:32:21 +0700

    feat: add getproduct by id and delete image product functions

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -10,8 +10,9 @@ import upload from '../libs/upload.js';
  * @tags Admin
 */
 async function getAllProducts(req, res) {
+    const { limit = 10, page = 1 } = req.query;
     try {
-        const products = await ProductsModel.getAllProducts();
+        const products = await ProductsModel.getAllProducts(limit, page);
         return sendResponse(res, 200, "Products retrieved successfully", products);
     } catch (error) {
         return sendResponse(res, 500, "Server error", null, error.message);
@@ -19,6 +20,26 @@ async function getAllProducts(req, res) {
 } //===============================================================================
 
 
+/**
+ * GET /admin/products/{id}
+ * @summary Retrieve a product by ID
+ * @tags Admin
+ * @param {string} id.path.required - Product ID
+ * @return {object} 200 - Product retrieved successfully
+ * @return {object} 404 - Product not found
+ */
+async function getProductById(req, res) {
+    const id = parseInt(req.params.id);
+    try {
+        const product = await ProductsModel.getProductById(id);
+        if (!product) {
+            return sendResponse(res, 404, "Product not found");
+        }
+        return sendResponse(res, 200, "Product retrieved successfully", product);
+    } catch (error) {
+        return sendResponse(res, 500, "Server error", null, error.message);
+    }
+}
 
 
 /**
@@ -26,40 +47,42 @@ async function getAllProducts(req, res) {
  * @summary Create a new product
  * @tags Admin
  * @param {string} title.formData.required - Product title
- * @param {string} description.formData.required - Product description
+ * @param {string} description.formData - Product description
  * @param {number} basePrice.formData.required - Product base price
  * @param {number} stock.formData.required - Product stock quantity
+ * @param {number} categoryId.formData - Product category ID
+ * @param {Array.<number>} sizes.formData - Array of size IDs to associate with the product
  * @return {object} 201 - Product created successfully
  * @return {object} 400 - Invalid input
-*/
-async function createProduct(req, res) {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        console.log(errors.array());
-        return sendResponse(res, 400, "Invalid input", null, errors.array());
-    }
-
+ * @return {object} 500 - Internal server error
+ */
+export const createProduct = async (req, res) => {
     try {
-        const { title, description, basePrice, stock, picture } = req.body;
+        const { title, description, basePrice, stock, categoryId, sizes } = req.body;
 
-        if (await ProductsModel.productIsExist(title)) {
-            return sendResponse(res, 400, "Product with this title already exists");
+        if (!title || !basePrice || stock === undefined) {
+            return sendResponse(res, 400, "Title, basePrice, and stock are required fields.");
         }
 
         const newProduct = await ProductsModel.createProduct({
             title,
             description,
-            basePrice,
-            stock,
-            picture
+            basePrice: parseFloat(basePrice),
+            stock: parseInt(stock),
+            categoryId: categoryId ? parseInt(categoryId) : null,
+            sizes: sizes || []
         });
-
-        sendResponse(res, 201, "Product created successfully", newProduct);
+        return sendResponse(res, 201, "Product created successfully", newProduct);
     } catch (error) {
-        sendResponse(res, 500, "Server error", null, error.message);
+        console.error("Error creating product:", error);
+
+        if (error.code === "P2002") {
+            return sendResponse(res, 400, "A product with the same title already exists.");
+        }
+
+        return sendResponse(res, 500, "Internal server error", null, error.message);
     }
-} //===============================================================================
+}; //===============================================================================
 
 
 
@@ -71,9 +94,9 @@ async function createProduct(req, res) {
 */
 
 /**
- * POST /admin/products/{id}/upload
+ * POST /admin/products/{id}/images
  * @summary Upload a picture for a product
- * @tags Products
+ * @tags Admin
  * @param {string} id.path.required - Product ID
  * @param {Upload} request.body.required - Picture file to upload - multipart/form-data
  * @return {object} 200 - Picture uploaded successfully
@@ -97,15 +120,41 @@ async function uploadPictureProduct(req, res) {
             }
 
             const updatedProduct = await ProductsModel.addProductImage(id, req.file.filename);
+            const product = await ProductsModel.getProductById(id);
 
-            return sendResponse(res, 200, "Picture uploaded successfully", updatedProduct);
+            return sendResponse(res, 200, "Picture uploaded successfully", product);
         });
     } catch (err) {
         return sendResponse(res, 500, "Server error", null, err.message);
     }
 } //===============================================================================
 
+/**
+ * DELETE /admin/{id}/images/{imgId}
+ * @summary Delete a picture for a product
+ * @tags Admin
+ * @param {string} id.path.required - Product ID
+ * @param {string} imgId.path.required - Image ID
+ * @returns {object} 200 - Image deleted successfully
+ * 
+ */
+async function deleteImageProducts(req, res) {
+    try {
+        const productId = parseInt(req.params.id);
+        const imageId = parseInt(req.params.imgId);
 
+        const product = await ProductsModel.getProductById(productId);
+        if (!product) {
+            return sendResponse(res, 404, "Product not found");
+        }
+
+        await ProductsModel.deleteProductImage(productId, imageId);
+        const updateProduct = await ProductsModel.getProductById(productId)
+        return sendResponse(res, 200, "Image deleted successfully", updateProduct);
+    } catch (error) {
+        return sendResponse(res, 500, "Server error", null, error.message);
+    }
+}
 
 
 /**
@@ -118,46 +167,55 @@ async function uploadPictureProduct(req, res) {
  * @param {number} basePrice.formData - Base price
  * @param {number} stock.formData - Stock quantity
  * @param {number} categoryId.formData - Category ID
+ * @param {Array.<number>} sizes.formData - Array of size IDs
  * @return {object} 200 - Product updated successfully
  * @return {object} 404 - Product not found
+ * @return {object} 500 - Server error
 */
 async function updateProduct(req, res) {
     try {
-        const id = parseInt(req.params.id);
+        const id = BigInt(req.params.id);
 
         const product = await ProductsModel.getProductById(id);
         if (!product) {
             return sendResponse(res, 404, "Product not found");
         }
 
-        const { title, description, basePrice, stock, categoryId, sizes } = req.body;
+        const { title, description, basePrice, stock, categoryId, sizes, variants } = req.body;
 
-        const updateData = { updatedAt: new Date() };
+        const updateData = { updated_at: new Date() };
 
         if (title !== undefined && title !== "") updateData.title = title;
         if (description !== undefined) updateData.description = description;
-        if (basePrice !== undefined) updateData.basePrice = parseFloat(basePrice);
+        if (basePrice !== undefined) updateData.base_price = parseFloat(basePrice);
         if (stock !== undefined) updateData.stock = parseInt(stock);
+
         if (categoryId !== undefined) {
-            updateData.categoryId = categoryId ? parseInt(categoryId) : null;
+            updateData.categories = categoryId
+                ? { connect: { id: parseInt(categoryId) } }
+                : { disconnect: true };
         }
 
-        // Update product
-        await ProductsModel.updateProduct(id, updateData);
-
-        // Update sizes
         if (Array.isArray(sizes)) {
             await ProductsModel.updateProductSizes(id, sizes);
         }
 
-        const newProduct = await ProductsModel.getProductById(id);
+        if (Array.isArray(variants)) {
+            console.log(variants);
+            await ProductsModel.updateProductVariants(id, variants);
+        }
 
+        await ProductsModel.updateProduct(id, updateData);
+
+        const newProduct = await ProductsModel.getProductById(id);
         return sendResponse(res, 200, "Product updated successfully", newProduct);
 
     } catch (error) {
+        console.error("Error updating product:", error);
         return sendResponse(res, 500, "Server error", null, error.message);
     }
 }
+
 //===============================================================================
 
 
@@ -185,5 +243,7 @@ export default {
     uploadPictureProduct,
     updateProduct,
     getAllProducts,
-    deleteProduct
+    deleteProduct,
+    getProductById,
+    deleteImageProducts
 };
