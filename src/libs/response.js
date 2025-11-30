ce0a26b46d16020fc797ba24d057f2749d5d88fd
siddiq24 <siddiqrm24@gmail.com>
2025-11-30 07:29:31 +0700

    feat: add serialize to error too

--- src/libs/response.js
+++ src/libs/response.js
@@ -1,11 +1,13 @@
-import serialize from "./serialize.js";
+import serialize from './serialize.js';
 
-function sendResponse(res, statusCode, message = "", data = null, error = null) {
+function sendResponse(res, statusCode, message, data = null, error = null) {
+    console.log(error);
     res.status(statusCode).json({
         success: statusCode >= 200 && statusCode < 300,
         message,
         data: serialize(data),
-        error: serialize(error)
+        error: serialize(error),
     });
 }
+
 export default sendResponse;
