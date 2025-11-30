dfb000e761548f7290f4bf2de53a779f5d91e3a2
siddiq24 <siddiqrm24@gmail.com>
2025-12-01 06:07:12 +0700

    feat: implement cors

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ import router from './src/routers/index.js';
 import express from 'express';
 import swaggerjsdoc from 'express-jsdoc-swagger';
 import cleanerFile from './src/libs/cleanerFile.js';
+import { connectToDatabase } from './src/libs/prisma.config.js';
+import { getRedisClient } from './src/libs/redis.config.js';
+import expressJSDocSwagger from 'express-jsdoc-swagger';
+import cors from 'cors'
 const port = 3000;
-
 const app = express();
+
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
+
+
 const option = {
     info: {
         version: '1.0.0',
@@ -19,14 +33,16 @@ const option = {
     swaggerUIPath: '/api-docs',
     baseDir: "__dirname"
 }
+const instance = expressJSDocSwagger(app)(option);
 
-cleanerFile()
+await connectToDatabase();
+await getRedisClient()
+await cleanerFile();
 swaggerjsdoc(app)(option);
 
 app.use(express.static('uploads'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.use('/', router);
 
