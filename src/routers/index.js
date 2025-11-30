1e73bbd71fb17b3af65eee70a19c6363ef0c8bc2
siddiq24 <siddiqrm24@gmail.com>
2025-12-01 06:04:07 +0700

    feat: add all entrypoint

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import authRouter from './auth.router.js';
 import adminRouter from './admin.router.js';
+import productRouter from './product.router.js';
+import cartRouter from './cart.router.js'
+import trxRouter from './transaction.router.js'
+import profileRouter from './profile.router.js'
 import { connectToDatabase } from '../libs/prisma.config.js';
 
 const router = express.Router();
 
-router.use('/admin', adminRouter);
 router.use('/auth', authRouter);
+router.use('/admin', adminRouter);
+router.use('/products', productRouter)
+router.use('/cart', cartRouter)
+router.use('/transactions', trxRouter)
+router.use('/profile', profileRouter)
 
 router.use('/', (req, res) => {
     res.json({
